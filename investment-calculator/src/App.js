90dddcd39aee3e66bff683dac3bcec1b599f8e62
danii0110 <105475360+danii0110@users.noreply.github.com>
2023-09-26 15:22:14 +0900

    .

--- investment-calculator/src/App.js
+++ investment-calculator/src/App.js
@@ -30,39 +30,37 @@ function App() {
 
   return (
     <div>
-      <header className="header">
-        <img src={logo} alt="logo" />
+      <header className='header'>
+        <img src={logo} alt='logo' />
         <h1>Investment Calculator</h1>
       </header>
 
-      <form className="form">
-        <div className="input-group">
+      <form className='form'>
+        <div className='input-group'>
           <p>
-            <label htmlFor="current-savings">Current Savings ($)</label>
-            <input type="number" id="current-savings" />
+            <label htmlFor='current-savings'>Current Savings ($)</label>
+            <input type='number' id='current-savings' />
           </p>
           <p>
-            <label htmlFor="yearly-contribution">Yearly Savings ($)</label>
-            <input type="number" id="yearly-contribution" />
-          </p>
-        </div>
-        <div className="input-group">
-          <p>
-            <label htmlFor="expected-return">
-              Expected Interest (%, per year)
-            </label>
-            <input type="number" id="expected-return" />
-          </p>
-          <p>
-            <label htmlFor="duration">Investment Duration (years)</label>
-            <input type="number" id="duration" />
+            <label htmlFor='yearly-contribution'>Yearly Savings ($)</label>
+            <input type='number' id='yearly-contribution' />
           </p>
         </div>
-        <p className="actions">
-          <button type="reset" className="buttonAlt">
+        <div className='input-group'>
+          <p>
+            <label htmlFor='expected-return'>Expected Interest (%, per year)</label>
+            <input type='number' id='expected-return' />
+          </p>
+          <p>
+            <label htmlFor='duration'>Investment Duration (years)</label>
+            <input type='number' id='duration' />
+          </p>
+        </div>
+        <p className='actions'>
+          <button type='reset' className='buttonAlt'>
             Reset
           </button>
-          <button type="submit" className="button">
+          <button type='submit' className='button'>
             Calculate
           </button>
         </p>
@@ -71,7 +69,7 @@ function App() {
       {/* Todo: Show below table conditionally (only once result data is available) */}
       {/* Show fallback text if no data is available */}
 
-      <table className="result">
+      <table className='result'>
         <thead>
           <tr>
             <th>Year</th>
