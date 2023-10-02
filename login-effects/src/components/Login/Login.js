2e57a928e19ac3aedae82f733e8e4515c7243d4a
danii0110 <105475360+danii0110@users.noreply.github.com>
2023-10-02 11:05:35 +0900

    .

--- login-effects/src/components/Login/Login.js
+++ login-effects/src/components/Login/Login.js
@@ -19,24 +19,27 @@ const Login = (props) => {
     };
   }, []);
 
-  useEffect(() => {
-    const identifier = setTimeout(() => {
-      console.log('Checking form validity!');
-      setFormIsValid(enteredEmail.includes('@') && enteredPassword.trim().length > 6);
-    }, 500);
+  // useEffect(() => {
+  //   const identifier = setTimeout(() => {
+  //     console.log('Checking form validity!');
+  //     setFormIsValid(enteredEmail.includes('@') && enteredPassword.trim().length > 6);
+  //   }, 500);
 
-    return () => {
-      console.log('CLEAN UP');
-      clearTimeout(identifier);
-    };
-  }, [enteredEmail, enteredPassword]);
+  //   return () => {
+  //     console.log('CLEAN UP');
+  //     clearTimeout(identifier);
+  //   };
+  // }, [enteredEmail, enteredPassword]);
 
   const emailChangeHandler = (event) => {
     setEnteredEmail(event.target.value);
+
+    setFormIsValid(event.target.value.includes('@') && enteredPassword.trim().length > 6);
   };
 
   const passwordChangeHandler = (event) => {
     setEnteredPassword(event.target.value);
+    setFormIsValid(enteredEmail.includes('@') && event.target.value.trim().length > 6);
   };
 
   const validateEmailHandler = () => {
