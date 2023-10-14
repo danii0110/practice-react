8aaf804c512e652f3c1027b5b769ee38ba1cc0d1
danii0110 <105475360+danii0110@users.noreply.github.com>
2023-10-14 16:46:24 +0900

    Making Items Removable

--- food-order-app/src/store/CartProvider.jsx
+++ food-order-app/src/store/CartProvider.jsx
@@ -16,7 +16,23 @@ const cartReducer = (state, action) => {
       totalAmount: updatedTotalAmount,
     };
   }
-  return defaultCartState;
+  if (action.type === 'REMOVE') {
+    const existingCartItemIndex = state.items.findIndex((item) => item.id === action.item.id);
+  }
+  const existingItem = state.items[existingCartItemIndex];
+  const updatedTotalAmount = state.totalAmout - existingItem.price;
+  let updatedItems;
+  if (existingItem.amount === 1) {
+    updatedItems = state.items.filter((item) => item.id !== action.id);
+  } else {
+    const updatedItem = { ...existingItem, amuont: existingItem.mount - 1 };
+    updatedItems = [...state.items];
+    updatedItems[existingCartItemIndex] = updatedItem;
+  }
+  return {
+    items: updatedItems,
+    totalAmount: updatedTotalAmount,
+  };
 };
 
 const CartProvider = (props) => {
